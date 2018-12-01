9b8487407936fe7f4daf12b848ea23755fcb16d3
ElRatonPoeta <cjuan14@gmail.com>
2018-12-01 17:29:20 -0400

    First Commit
    First Commit After various changes. To view all changes to the TBS Framework, do a Beyond Compare with the TBS Framework Clean Copy Scripts. This is a prototyping Unity 3D project, with various things contained within. These are, the Brackeys Tower Defense tutorial game. Quick outline script from the unity asset store.

---
+++ TurnBasedFrameWork/Assets/_Experiments/CustomObjectiveGenerator.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class CustomObjectiveGenerator : MonoBehaviour {
+
+
+    public Transform ObjectivesParent;
+    public GameObject objectivePrefab;
+    public CellGrid CellGrid;// for inspector snap to grid
+
+    Queue<Cell> shuffledCells;
+    public int seed = 10;
+
+    public void RandomSpawnObjective(List<Cell> cellsFromCellGridObj)
+    {
+        shuffledCells = new Queue<Cell>(Utility.ShuffleArray(cellsFromCellGridObj.ToArray(), seed));
+        CreateObjectiveInObjectivesParent();
+
+        for (int i = 0; i < ObjectivesParent.childCount; i++)
+        {
+            var objective = ObjectivesParent.GetChild(i);// I need a function to Create the objectives Childs first.
+            var cell = cellsFromCellGridObj.OrderBy(h => Math.Abs((h.transform.position - objective.transform.position).magnitude)).First();
+            if (!cell.IsTaken)
+            {
+                cell.IsTaken = true;
+                var bounds = getBounds(objective);
+                Vector3 offset = new Vector3(0, bounds.y + 1f, 0);
+                objective.localPosition = cell.transform.localPosition + offset;
+            }
+            else
+            {
+                Debug.Log("When spawing an objective found a cell which is taken");
+                Destroy(objective.gameObject);
+            }
+
+        }
+    }
+
+    public void CreateObjectiveInObjectivesParent()
+    {
+        Vector3 randomCoord = GetRandomCellPosition();
+        Transform newObjective = Instantiate(objectivePrefab.transform, randomCoord, Quaternion.identity) as Transform;
+        newObjective.parent = ObjectivesParent;
+    }
+
+    public Vector3 GetRandomCellPosition()
+    {
+        Cell randomCell = shuffledCells.Dequeue();
+        while(randomCell.IsTaken)
+        {
+            shuffledCells.Enqueue(randomCell);
+            randomCell = shuffledCells.Dequeue();
+        }
+        // Out of while therefore, have a random cell that is not taken.
+        Vector3 randomCoord = randomCell.transform.position;
+        shuffledCells.Enqueue(randomCell);
+        return randomCoord;
+    }
+
+    void Parent(GameObject parentOb, GameObject childOb)
+    {
+        childOb.transform.parent = parentOb.transform;
+    }
+
+    private Vector3 getBounds(Transform _transform)
+    {
+        var _renderer = _transform.GetComponent<Renderer>();
+        var combinedBounds = _renderer.bounds;
+        var renderers = _transform.GetComponentsInChildren<Renderer>();
+        foreach (var childRenderer in renderers)
+        {
+            if (childRenderer != _renderer) combinedBounds.Encapsulate(childRenderer.bounds);
+        }
+
+        return combinedBounds.size;
+    }
+
+}
---
+++ TurnBasedFrameWork/Assets/_Experiments/TurnTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TurnTracker : MonoBehaviour {
+
+    public int CurrentTurn = 0;
+    public CellGrid _cellGrid;
+
+    private void Start()
+    {
+        _cellGrid.GameStarted += IncreaseTurn;
+        _cellGrid.TurnEnded += IncreaseTurn;
+        _cellGrid.GameEnded += ResetTurnTracker;
+    }
+
+    private void ResetTurnTracker(object sender, EventArgs e)
+    {
+        Debug.Log("Called ResetTurnTracker");
+        CurrentTurn = 0;
+    }
+
+    private void IncreaseTurn(object sender, EventArgs e)
+    {
+        CurrentTurn++;
+        Debug.Log("Im increasing the current turn current turn value = " + CurrentTurn.ToString());
+    }
+}
